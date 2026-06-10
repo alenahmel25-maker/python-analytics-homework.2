12388c8370d80e12f2d0f5d7dd40059621114e7e
Helen <alenahmel25@gmail.com)>
2026-06-10 11:30:32 +0300

    temporary title change

--- analysis2.py
+++ analysis2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 student = {"name": ["Eva", "Polina", "Sanek"], "point": [194, 191, 185]}
 df = pd.DataFrame(student)
-print("Кількість набраних балів НМТ:")
+print("Кількість набраних балів НМТ (тимчасова версія):")
 print(df)
 print("Середнє значення:", df["point"].mean())
